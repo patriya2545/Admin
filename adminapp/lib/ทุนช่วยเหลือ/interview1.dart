d403038525641e0eaa8d4be6f89d4cdeca69e946
pattariya <patriya2545@gmail.com>
2023-02-22 00:02:35 +0700

    interview1

--- adminapp/lib/ทุนช่วยเหลือ/interview1.dart
+++ adminapp/lib/ทุนช่วยเหลือ/interview1.dart
@@ -17,6 +17,12 @@ class _Interview1State extends State<Interview1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Color.fromARGB(255, 221, 139, 236,),
+        title: 
+          Text("รายชื่อผู้ผ่านการสมัครทุน",
+          style: TextStyle(fontSize: 25),),),
+          
       body: Padding(
         padding: const EdgeInsets.all(30.0),
           child: Center(
