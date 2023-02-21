ec7d22d2d1576f840816f3c2d4329c4633a1f533
pattariya <patriya2545@gmail.com>
2023-02-22 00:01:05 +0700

    examine22

--- adminapp/lib/ทุนช่วยเหลือ/examine22.dart
+++ adminapp/lib/ทุนช่วยเหลือ/examine22.dart
@@ -1,4 +1,9 @@
 import 'package:adminapp/%E0%B8%97%E0%B8%B8%E0%B8%99%E0%B8%8A%E0%B9%88%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%AB%E0%B8%A5%E0%B8%B7%E0%B8%AD/examine33.dart';
+import 'package:adminapp/%E0%B8%97%E0%B8%B8%E0%B8%99%E0%B8%95%E0%B8%B2%E0%B8%A1%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B9%8C/interview.dart';
+import 'package:adminapp/capital.dart';
+import 'package:adminapp/examine.dart';
+import 'package:adminapp/main.dart';
+import 'package:adminapp/result.dart';
 import 'package:flutter/material.dart';
 
 //รายชื่อผู้สมัครขอทุนช่วยเหลือค่าครองชีพ
@@ -14,6 +19,12 @@ class _Examine22State extends State<Examine22> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Color.fromARGB(255, 221, 139, 236,),
+        title: 
+          Text("รายชื่อผู้สมัครทุน",
+          style: TextStyle(fontSize: 25),),),
+          
       body: Padding(
         padding: const EdgeInsets.all(30.0),
           child: Center(
@@ -89,3 +100,4 @@ class _Examine22State extends State<Examine22> {
     );
   }
 }
+
